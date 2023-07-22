9142d899d417fd86a16bfcc25aa515b2cf03050f
Stream Protocol Ltd <streamprotocol@protonmail.com>
2023-07-22 07:36:51 +0000

    minor tweaks

--- ui/src/components/organisms/login-card/index.tsx
+++ ui/src/components/organisms/login-card/index.tsx
@@ -47,7 +47,7 @@ const LoginCard = ({ toResetPassword }: LoginCardProps) => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="flex flex-col items-center">
         <h1 className="inter-xlarge-semibold text-grey-90 mb-large text-[20px]">
-          Log in to StreamCommerce
+          Log in
         </h1>
         <div>
           <SigninInput
